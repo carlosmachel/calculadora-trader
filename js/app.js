97d118233dcfe52219184d28c4baea03dfb40f1b
Carlos Machel <carlos.git@outlook.com>
2018-05-04 12:03:14 -0300

    alteração no layout e implementação da  interação com o DOM.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function (win, doc) {
   "use strict";
 
   function app() {
@@ -47,12 +47,83 @@
       operacao: "compra"
     });
 
+    function Estrategia(nome, pontosGain, pontosLoss) {
+      this.nome = nome;
+      this.pontosGain = pontosGain;
+      this.pontosLoss = pontosLoss;
+    }
+
+    var estrategias = [new Estrategia('150x100', 150, 100)];
+
     var ordensFinalizadas = [];
     var ordensLog = [];
     var operacao;
     var operacoes = { semOperacao: -1, compra: 1, venda: 0 };
     var fluxo = ["abertura", "fechamento", "cancelamento"];
+
     var ativos = { miniIndice: 0.2, miniDolar: 10 };
+
+
+    function popularAtivos() {
+      var $selectAtivos = doc.querySelector("[data-js='ativos']");
+
+      Object.keys(ativos).forEach(function (ativo) {
+        var $option = doc.createElement('option');
+        $option.value = ativos[ativo];
+        $option.innerHTML = ativo;
+        $selectAtivos.appendChild($option);
+      });
+    }
+
+    function popularEstrategias() {
+      var $selectEstrategias = doc.querySelector("[data-js='estrategias']");
+
+      estrategias.forEach(function (estrategia) {
+        var $option = doc.createElement('option');
+        $option.value = estrategia.nome;
+        $option.innerHTML = estrategia.nome;
+        $selectEstrategias.appendChild($option);
+      });
+    }
+
+    function popularTabelaOrdens(element) {
+      var $tableBody = doc.querySelector('[data-js="ordens"]');
+
+      var $tr = doc.createElement('tr');
+
+      var $contratos = doc.createElement('td');
+      $contratos.innerHTML = 'contratos';
+      $tr.appendChild($contratos);
+      var $preco = doc.createElement('td');
+      $preco.innerHTML = 'contratos';
+      $tr.appendChild($preco);
+      var $gain = doc.createElement('td');
+      $gain.innerHTML = 'contratos';
+      $tr.appendChild($gain);
+      var $loss = doc.createElement('td');
+      $loss.innerHTML = 'contratos';
+      $tr.appendChild($loss);
+      var $abertura = doc.createElement('td');
+      $abertura.innerHTML = 'contratos';
+      $tr.appendChild($abertura);
+      var $fechamento = doc.createElement('td');
+      $fechamento.innerHTML = 'contratos';
+      $tr.appendChild($fechamento);
+      var $cancelar = doc.createElement('td');
+      $cancelar.innerHTML = 'contratos';
+      $tr.appendChild($cancelar);
+
+      $tableBody.appendChild($tr);
+    }
+
+    var $form = doc.querySelector('[data-js="form"]');
+
+    $form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      popularTabelaOrdens(this);
+    });
+
 
     var quantidadeDeContratosOperacao = 0;
     var precoMedio = 0;
@@ -77,22 +148,42 @@
       };
     })();
 
-    function calcularMeta(metaEmReal, ativo) {
-      return metaEmReal ? metaEmReal / ativos[ativo] : 0;
+
+    var $meta = doc.querySelector('[data-js="meta"]');
+    $meta.addEventListener('blur', calcularMeta);
+
+    function calcularMeta() {
+      var metaEmReal = this.value;
+      var ativo = doc.querySelector('[data-js="ativos"] > option:checked');
+
+      var metaEmPontos = doc.querySelector('[data-js="metaEmPontos"]');
+
+      metaEmPontos.innerHTML = `${metaEmReal ? metaEmReal / ativo.value : 0} pts`;
     }
 
-    function calcularLimite(limiteEmReal, ativo) {
-      return limiteEmReal ? limiteEmReal / ativos[ativo] : 0;
+    var $limite = doc.querySelector('[data-js="limite"]');
+    $limite.addEventListener('blur', calcularLimite);
+
+    function calcularLimite() {
+      var limiteEmReal = this.value;
+      var ativo = doc.querySelector('[data-js="ativos"] > option:checked');
+
+      var limiteEmPontos = doc.querySelector('[data-js="limiteEmPontos"]');
+
+      limiteEmPontos.innerHTML = `${limiteEmReal ? limiteEmReal / ativo.value : 0} pts`;
     }
 
     function contabilizarOrdens(ativo, ordem, fluxo) {
+      var precoGain = 0;
+      var precoLoss = 0;
+
       if (precoMedio) {
         if (fluxo === "abertura") {
           contabilizarAbertura(ativo, ordem);
         } else if (fluxo === "fechamento") {
           contabilizarFechamento(ativo, ordem);
         } else {
-          contabilizarStop(ativo, ordem);
+          contabilizarStop(ativo, ordem); // não está certo ainda qual a operação.
         }
       } else {
         precoMedio = ordem.preco;
@@ -102,17 +193,17 @@
 
       if (operacao === "compra") {
         precoGain = quantidadeDeContratosOperacao
-          ? precoMedio + metaEmPontos / quantidadeDeContratosOperacao
+          ? precoMedio + (metaEmPontos / quantidadeDeContratosOperacao)
           : 0;
         precoLoss = quantidadeDeContratosOperacao
-          ? precoMedio - limiteLossEmPontos / quantidadeDeContratosOperacao
+          ? precoMedio - (limiteLossEmPontos / quantidadeDeContratosOperacao)
           : 0;
       } else {
         precoGain = quantidadeDeContratosOperacao
-          ? precoMedio - metaEmPontos / quantidadeDeContratosOperacao
+          ? precoMedio - (metaEmPontos / quantidadeDeContratosOperacao)
           : 0;
         precoLoss = quantidadeDeContratosOperacao
-          ? precoMedio + limiteLossEmPontos / quantidadeDeContratosOperacao
+          ? precoMedio + (limiteLossEmPontos / quantidadeDeContratosOperacao)
           : 0;
       }
 
@@ -123,7 +214,7 @@
     }
 
     function atualizarPrecoOrdens(preco) {
-      ordens = ordens.forEach(function(item) {
+      ordens = ordens.forEach(function (item) {
         if (item.aberta) item.preco = preco;
       });
     }
@@ -139,14 +230,14 @@
         precoMedio =
           precoMedio * quantidadeDeContratosOperacao +
           ordem.preco *
-            quantidadeDeContratosAbertura /
-            (quantidadeDeContratosOperacao + quantidadeDeContratosAbertura);
+          quantidadeDeContratosAbertura /
+          (quantidadeDeContratosOperacao + quantidadeDeContratosAbertura);
 
         quantidadeDeContratosOperacao += quantidadeDeContratosAbertura;
         ordem.aberta = true;
 
         if (ordem.escalonado) {
-          ordens = ordens.map(function(item) {
+          ordens = ordens.map(function (item) {
             if (
               item.preco === ordem.preco &&
               !item.aberta &&
@@ -165,14 +256,14 @@
         precoMedio +=
           ordem.operacao === "compra"
             ? ordem.quantidadeDeContratos *
-              ((ordem.precoGain - precoMedio) * -1)
+            ((ordem.precoGain - precoMedio) * -1)
             : ordem.precoGain - precoMedio;
 
         quantidadeDeContratosOperacao -= ordem.quantidadeDeContratos;
       }
 
       ordem.fechada = true;
-      ordens = ordens.filter(function(item) {
+      ordens = ordens.filter(function (item) {
         return item.index !== ordem.index;
       });
 
@@ -183,7 +274,7 @@
 
     function contabilizarStop(ordem) {
       ordens = ordens
-        .map(function(item) {
+        .map(function (item) {
           if (item.operacao === operacao && item.aberta) {
             item.preco = precoMedio;
             item.precoGain = ordem.preco;
@@ -192,7 +283,7 @@
           }
           return item;
         })
-        .filter(function(item) {
+        .filter(function (item) {
           return item !== undefined;
         });
 
@@ -205,12 +296,12 @@
     function calcularQuantidadeDeContratosAbertura(ordem) {
       return ordem.escalonado
         ? ordens
-            .filter(function(item) {
-              return item.escalonado && item.operacao === ordem.operacao;
-            })
-            .reduce(function(acumulated, currentOrdem) {
-              return acumulated + currentOrdem.quantidadeDeContratos;
-            }, 0)
+          .filter(function (item) {
+            return item.escalonado && item.operacao === ordem.operacao;
+          })
+          .reduce(function (acumulated, currentOrdem) {
+            return acumulated + currentOrdem.quantidadeDeContratos;
+          }, 0)
         : ordem.quantidadeDeContratos;
     }
 
@@ -224,7 +315,10 @@
       },
       "abertura"
     );
+
+    popularAtivos();
+    popularEstrategias();
   }
 
   app();
-})();
+})(window, document);
